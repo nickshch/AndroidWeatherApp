0dbe6bc3db7ad83bcde1d7235391df3cf539c643
Nick Shch <ndnd@mail.ru>
2016-10-23 13:22:43 +0300

    Added city select activity

---
+++ app/src/main/java/nickshch/androidweatherapp/CitySelectActivity.java
@@ -0,0 +1,13 @@
+package nickshch.androidweatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CitySelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_select);
+    }
+}
